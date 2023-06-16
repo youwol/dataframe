17725ab98450603aa1a4cf489d2168abd9446098
youwol-guillaume <greinisch@youwol.com>
2023-06-16 07:59:29 +0200

    🎨 python config files => apply `black` formatting

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -6,14 +6,17 @@ from youwol.utils.context import Context
 
 
 class PipelineFactory(IPipelineFactory):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     async def get(self, _env: YouwolEnvironment, context: Context):
-        config = PipelineConfig(target=JsBundle(links=[
-            Link(name="doc", url="dist/docs/modules/lib.html"),
-            Link(name="coverage", url="coverage/lcov-report/index.html"),
-            Link(name="bundle-analysis", url="dist/bundle-analysis.html")
-        ]))
+        config = PipelineConfig(
+            target=JsBundle(
+                links=[
+                    Link(name="doc", url="dist/docs/modules/lib.html"),
+                    Link(name="coverage", url="coverage/lcov-report/index.html"),
+                    Link(name="bundle-analysis", url="dist/bundle-analysis.html"),
+                ]
+            )
+        )
         return await pipeline(config, context)
--- template.py
+++ template.py
@@ -14,43 +14,38 @@ from youwol.utils import parse_json
 
 folder_path = Path(__file__).parent
 
-pkg_json = parse_json(folder_path / 'package.json')
+pkg_json = parse_json(folder_path / "package.json")
 
 
 template = Template(
     path=folder_path,
     type=PackageType.Library,
-    name=pkg_json['name'],
-    version=pkg_json['version'],
-    shortDescription=pkg_json['description'],
-    author=pkg_json['author'],
-    dependencies=Dependencies(
-        runTime=RunTimeDeps(
-            externals={}
-        ),
-        devTime={}
-    ),
+    name=pkg_json["name"],
+    version=pkg_json["version"],
+    shortDescription=pkg_json["description"],
+    author=pkg_json["author"],
+    dependencies=Dependencies(runTime=RunTimeDeps(externals={}), devTime={}),
     bundles=Bundles(
-        mainModule=MainModule(
-            entryFile="./lib/index.ts",
-            loadDependencies=[]
-        )
+        mainModule=MainModule(entryFile="./lib/index.ts", loadDependencies=[])
     ),
-    userGuide=True
+    userGuide=True,
 )
 
 generate_template(template)
 
 shutil.copyfile(
-    src=folder_path / '.template' / 'src' / 'auto-generated.ts',
-    dst=folder_path / 'src' / 'auto-generated.ts'
+    src=folder_path / ".template" / "src" / "auto-generated.ts",
+    dst=folder_path / "src" / "auto-generated.ts",
 )
-for file in ['.gitignore', 'README.md', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'webpack.config.ts',
-             #  'typedoc.js'
-             ]:
-    shutil.copyfile(
-        src=folder_path / '.template' / file,
-        dst=folder_path / file
-    )
-
+for file in [
+    ".gitignore",
+    "README.md",
+    ".npmignore",
+    ".prettierignore",
+    "LICENSE",
+    "package.json",
+    "tsconfig.json",
+    "webpack.config.ts",
+    #  'typedoc.js'
+]:
+    shutil.copyfile(src=folder_path / ".template" / file, dst=folder_path / file)
